3ce4aef462c954a19e21de86fd7bf67cafcbf9cb
EdgarValcarcel <edgarvalcarcel@hotmail.com>
2024-02-03 11:06:49 -0500

    No authenticationScheme added

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using ProductMaster.Application.Common.Interfaces;
 using ProductMaster.Infrastructure.Data;
 using ProductMaster.Infrastructure.Data.Interceptors;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -24,6 +28,30 @@ public static class DependencyInjection
 
         services.AddScoped<IProductMasterDbContext>(provider => provider.GetRequiredService<ProductMasterDbContext>());
         services.AddScoped<ProductMasterDbContextInitialiser>();
+
+
+        services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+        services.AddAuthorizationBuilder();
+
+
+        //services.AddAuthentication(options =>
+        //{
+        //    options.DefaultAuthenticateScheme = DefaultAuthenticationTypes.ApplicationCookie;
+        //})
+        //.AddCookie(DefaultAuthenticationTypes.ApplicationCookie, options =>
+        //{
+
+        //    options.LoginPath = "/Login";
+        //    options.LogoutPath = "/Logout";
+        //});
+        services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme,
+        options =>
+        {
+            options.LoginPath = new PathString("/auth/login");
+            options.AccessDeniedPath = new PathString("/auth/denied");
+        });
+
         services.AddSingleton(TimeProvider.System);
         return services;
     }
